f95c91c9906c7712977955f0d0c76af2b7ebaa26
Rodrigo Torres <rodrigohtorres@hotmail.com>
2022-09-06 20:14:33 -0300

    Initial commit

---
+++ grid/gridApp.swift
@@ -0,0 +1,17 @@
+//
+//  gridApp.swift
+//  grid
+//
+//  Created by Rodrigo Torres on 07/08/22.
+//
+
+import SwiftUI
+
+@main
+struct gridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
